e461a532f473f2876e752a26bd6e72d6fbb541be
elijah grapes <ooisee@gmail.com>
2015-01-27 02:29:01 +0500

    fix tests (tottoro)

--- test/test_tottoro.bat
+++ test/test_tottoro.bat
@@ -1,1 +1,1 @@
-ruby autosiril.rb tottoro_example.mid 1d-2me-3p,4m[uf]-5m[2]+,5m[6]-6me[2]+-3p[3]+-2mew+ 8 6 12 0 64 2 6
+ruby ../autosiril.rb tottoro_example.mid 1d-2me-3p,4m[uf]-5m[2]+,5m[6]-6me[2]+-3p[3]+-2mew+ 8 6 12 0 64 2 6
